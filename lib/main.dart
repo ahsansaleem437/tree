95a96e3a82f02f93e222e0bbed3ad67d798050a0
ahsansaleem437 <ahsansleem28@gmail.com>
2024-07-04 15:30:44 +0500

    tree

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Scaffold(
+    body:Column(
+      children: [
+        Container(
+          width:100,
+          color: Colors.blue,
+        ),
+      ],
+    )
+
+  ),
+));
